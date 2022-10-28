131f096827e99811839bf8b1562cdef9e41cd198
Justin Standish-White <justinstandish.white@gmail.com>
2022-10-28 18:54:06 +0200

    Formatted probabilities table so it is easy to access data. There's an example of how we will generate the food/drink for a given time

--- Code/Simulation.py
+++ Code/Simulation.py
@@ -7,13 +7,12 @@ import os
 
 # Setting up database of times with menu probabilities
 
-food_prob = pd.DataFrame(columns = ['Time', 'Cookie', 'Muffin', 'Pie', 'Sandwich', 'None'])
-drinks_prob = pd.DataFrame(columns = ['Time', 'Coffee', 'Frappucino', 'Milkshake', 'Soda', 'Tea', 'Water'])
+food_prob = pd.DataFrame(columns = ['Cookie', 'Muffin', 'Pie', 'Sandwich', 'None'])
+drinks_prob = pd.DataFrame(columns = ['Coffee', 'Frappucino', 'Milkshake', 'Soda', 'Tea', 'Water'])
 
 timedelta = dt.timedelta(hours = 8)
 incr = dt.timedelta(minutes = 1)
 
-ind = 1
 
 while timedelta < dt.timedelta(hours = 18):
 
@@ -21,75 +20,63 @@ while timedelta < dt.timedelta(hours = 18):
     hour = time.hour
 
     if hour < 11:
-        food_i = pd.DataFrame([[time, 0, 0, 0, 0, 1]],
-                              columns = ['Time', 'Cookie', 'Muffin', 'Pie', 'Sandwich', 'None'], index = [ind])
+        food_i = pd.DataFrame([[0, 0, 0, 0, 1]],
+                              columns = ['Cookie', 'Muffin', 'Pie', 'Sandwich', 'None'], index = [time])
         food_prob = pd.concat([food_prob, food_i])
 
-        drinks_i = pd.DataFrame([[time, 1 / 3, 2 / 15, 2 / 15, 2 / 15, 2 / 15, 2 / 15]],
-                                columns=['Time', 'Coffee', 'Frappucino', 'Milkshake', 'Soda', 'Tea', 'Water'], index = [ind])
+        drinks_i = pd.DataFrame([[1/3, 2/15, 2/15, 2/15, 2/15, 2/15]],
+                                columns=['Coffee', 'Frappucino', 'Milkshake', 'Soda', 'Tea', 'Water'], index = [time])
         drinks_prob = pd.concat([drinks_prob, drinks_i])
 
     elif (hour >= 11) & (hour < 13):
-        food_i = pd.DataFrame([[time, 1/8, 1/8, 1/8, 5/8, 0]],
-                              columns=['Time', 'Cookie', 'Muffin', 'Pie', 'Sandwich', 'None'], index = [ind])
+        food_i = pd.DataFrame([[1/8, 1/8, 1/8, 5/8, 0]],
+                              columns=['Cookie', 'Muffin', 'Pie', 'Sandwich', 'None'], index = [time])
         food_prob = pd.concat([food_prob, food_i])
 
-        drinks_i = pd.DataFrame([[time, 1/12, 1/12, 1/12, 7/12, 1/12, 1/12]],
-                                columns=['Time', 'Coffee', 'Frappucino', 'Milkshake', 'Soda', 'Tea', 'Water'], index=[ind])
+        drinks_i = pd.DataFrame([[1/12, 1/12, 1/12, 7/12, 1/12, 1/12]],
+                                columns=['Coffee', 'Frappucino', 'Milkshake', 'Soda', 'Tea', 'Water'], index=[time])
         drinks_prob = pd.concat([drinks_prob, drinks_i])
 
     else:
-        food_i = pd.DataFrame([[time, 4/30, 4/30, 4/30, 0, 3/5]],
-                              columns=['Time', 'Cookie', 'Muffin', 'Pie', 'Sandwich', 'None'], index = [ind])
+        food_i = pd.DataFrame([[4/30, 4/30, 4/30, 0, 3/5]],
+                              columns=['Cookie', 'Muffin', 'Pie', 'Sandwich', 'None'], index = [time])
         food_prob = pd.concat([food_prob, food_i])
 
-        drinks_i = pd.DataFrame([[time, 1/6, 1/6, 1/6, 1/6, 1/6, 1/6]],
-                                columns=['Time', 'Coffee', 'Frappucino', 'Milkshake', 'Soda', 'Tea', 'Water'], index=[ind])
+        drinks_i = pd.DataFrame([[1/6, 1/6, 1/6, 1/6, 1/6, 1/6]],
+                                columns=['Coffee', 'Frappucino', 'Milkshake', 'Soda', 'Tea', 'Water'], index=[time])
         drinks_prob = pd.concat([drinks_prob, drinks_i])
 
     print(timedelta)
     timedelta = timedelta + incr
-    ind = ind + 1
 
 # CSV's to check
 food_prob.to_csv('Results/Food_Prob.csv')
 drinks_prob.to_csv('Results/Drinks_Prob.csv')
 
 
+# Adapting indexes to easy access later on
+times_idx = pd.period_range("2000-01-01 8:00", freq="T", periods = 600)
+food_prob.index = times_idx
+drinks_prob.index = times_idx
+
+# allows us to easily select specific rows
+# for example, for food
+
+test_time = dt.time(hour=16, minute=14)
+
+row_ind = ((food_prob.index.minute == test_time.minute) & (food_prob.index.hour == test_time.hour))
+probs_row = food_prob[row_ind]
+
+options = list(probs_row.columns)
+probs = probs_row.values.tolist()[0]
+
+food = np.random.choice(options, 1, p = probs)
+print(food)
 
 # Prices menus
 
 food_menu = pd.DataFrame( [2, 3, 3, 2, 0], columns = ['Price'], index=['Cookie', 'Muffin', 'Pie', 'Sandwich', 'None'])
 drinks_menu = pd.DataFrame( [3, 4, 5, 3, 3, 2], columns = ['Price'], index=['Coffee', 'Frappucino', 'Milkshake', 'Soda', 'Tea', 'Water'])
 
-elements = ["A", "B"]
-probs = [.1,.9]
-x = np.random.choice(elements, 1, p = probs)
-print(type(x[0]))
-
-df = pd.DataFrame(np.random.randn(26,5), columns=['col'+str(i) for i in range(5)],index=list("ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
-df['cat'] = list('aaaabbbccddef' * 2)
-
-idx = pd.period_range('2013-01', periods=len(df), freq='M')
-df.index = idx
-february_selector = (df.index.month == 2)
-february_data = df[february_selector]
-
-q1_data = df[(df.index.month >= 1) & (df.index.month <= 3)] # note: & not "and"
-mayornov_data = df[(df.index.month == 5) | (df.index.month == 11)] # note: | not "or"
-annual_tot = df.groupby(df.index.year).sum()
 
 
-print((food_prob[food_prob.index.hour == 8]) # == dt.time(hour = 17, minute = 59))
-
-times_practice = []
-for hour in range(8,18):
-    for minute in range(0, 60, 1):
-        times_practice.append(dt.time(hour = hour, minute=minute))
-
-print(len(times_practice))
-
-df2 = pd.DataFrame(np.random.randn(600), columns=['val'])
-df2.index = times_practice
-
-sub1 = (df2.index. == 8)
