c769cc62069be143b4ffa0be614b5f3d696a7d0b
Justin Standish-White <justinstandish.white@gmail.com>
2022-10-29 17:04:08 +0200

    FInalised menus

--- Code/Simulation.py
+++ Code/Simulation.py
@@ -12,7 +12,7 @@ food_menu = pd.DataFrame([['Cookie', 2, 0, 1/8, 4/30],
                           ['Pie', 3,0,1/8,4/30],
                           ['Sandwich',2,0,5/8,0],
                           ['None', 0,1,0,3/5]],
-                         columns = ['food_item', 'price', 'breakfast_prob', 'lunch_prob', 'dinner_prob'],
+                         columns=['food_item', 'price', 'breakfast_prob', 'lunch_prob', 'dinner_prob'],
                          )
 
 drinks_menu = pd.DataFrame([['Coffee',   3, 1/3,  1/12, 1/6],
